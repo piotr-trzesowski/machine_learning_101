11fa26544fe0b774c82e1dba739c2b8aa656bd4e
piotrtrzesowski <piotr.trzesowski@icloud.com>
2025-08-21 17:42:17 +0200

    Add corrections, add context manager.

--- train_ai_to_play_mario_PPO_algorithm.py
+++ train_ai_to_play_mario_PPO_algorithm.py
@@ -1,21 +1,14 @@
-# Not working - in progress
-
-import gymnasium as gym  # <-- Use this instead of 'import gym'
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
-env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
-
-obs, info = env.reset()  # Note: Gymnasium returns (obs, info) instead of just obs
-done = False
-while not done:
-    action = env.action_space.sample()
-    obs, reward, terminated, truncated, info = env.step(action)  # Gymnasium uses 'terminated' and 'truncated'
-    done = terminated or truncated
-    env.render()
-
-env.close()
+with gym_super_mario_bros.make('SuperMarioBros-1-1-v0') as env:
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    obs = env.reset()
+    done = False
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
 
 # Note: The above code is a simple example of how to set up and run a Super Mario Bros environment using Gymnasium.
